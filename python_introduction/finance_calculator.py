9bdb860d3b1628847f0a11bce263f52969906975
MSEC <msec@localhost.localdomain>
2025-07-11 19:56:19 +0000

    annual projected savings calculator in python

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = input("Enter your monthly income: ")
-total_monthly_expenses = input("Enter your total monthly expenses: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
-monthly_savings = float(monthly_income) - float(total_monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)  # Assuming a 5% annual interest rate
 

75ef4de00c7b5c2bd8627d1a8c27d32fb97e6094
MSEC <msec@localhost.localdomain>
2025-07-20 09:40:53 +0000

    created a match case calculator app in python

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+operation = input("Enter the operation (+, -, *, /): ")
+
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "cannot divide by zero"
+
+print(f"The result is: {result}")

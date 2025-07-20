627263c56543b421baf0577d1ed92c2dd6d77724
MSEC <msec@localhost.localdomain>
2025-07-20 11:35:40 +0000

    created a daily reminder app using python

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,8 +1,8 @@
 task = input("Enter your task: ")
-task_priority = input("Priority: (high, medium, or low)").lower()
+priority = input("Priority: (high/medium/low)").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-match task_priority:
+match priority:
     case "high" if time_bound == "yes":
         print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
     case "high" if time_bound == "no":

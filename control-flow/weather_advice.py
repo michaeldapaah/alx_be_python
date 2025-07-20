27196b39d409fbfe1d1ae67a5e48e137968d950a
MSEC <msec@localhost.localdomain>
2025-07-20 09:22:48 +0000

    created a weather advisory app in python

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if current_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 
-elif current_weather.lower() == "rainy":
+elif weather.lower() == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 
-elif current_weather.lower() == "cold":
+elif weather.lower() == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 
 else:

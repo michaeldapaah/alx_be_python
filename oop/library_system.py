07cbc5ae3151a52cd9def10619110b2db121c8a4
MSEC <msec@localhost.localdomain>
2025-08-10 21:23:05 +0000

    working on library system in python making emphasis on inheritance and composition:

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, file_size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
